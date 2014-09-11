908709069c780117d5a26f91755f5bfdbaf65254
chimng <timothyngt@gmail.com>
2014-09-11 01:11:58 -0400

    Removed unnecessary function and replaced ambiguous variables

--- search.js
+++ search.js
@@ -8,23 +8,16 @@ $(document).ready(function() {
 
 function search(searchItem) {
 	//return "This is the string from search function";
-	var search = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 	var url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + searchItem + "?api_key=cc414580-c562-4f62-a74a-a1a8076e7a59";
-	search.open("GET", url, false);
-	search.send();
+	xhr.open("GET", url, false);
+	xhr.send();
 	//var myObject = JSON.stringify(search);
 	//var obj = JSON.parse(myObject);
-	var obj = JSON.parse(search.response);
+	var obj = JSON.parse(xhr.response);
 	//var finalobj = JSON.parse(obj.response);
 
 	
 	return searchItem + ": " + obj[searchItem].id;
 	
-}
-
-function replacer(key, value) {
-    if (typeof value === 'number' && !isFinite(value)) {
-        return String(value);
-    }
-    return value;
 }
