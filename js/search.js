6cd3a51fa869aef2ed4aa0c288b89117a389d49d
themantello <thechoy@gmail.com>
2014-09-12 14:07:44 -0400

    Changed search Function to return unparsed JSON
    1. Change search Function to return unparsed JSON
    2. Modified Output to show summer Icon, name, and level.

--- js/search.js
+++ js/search.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
     $('button').click(function() {
     	var toSearch = $("input[name=searchitem]").val();
-    	var toAdd = search(toSearch)
-        $('#results').append("<p>"+toAdd+"</p>");
+    	var summonerObj = JSON.parse(search(toSearch));
+    	var sumIcon = summonerObj[toSearch].profileIconId; 
+    	var toAdd = summonerObj[toSearch].name;
+        $('#results').append("<h1> <img src=\"https://ddragon.leagueoflegends.com/cdn/4.16.1/img/profileicon/"+sumIcon+".png\"> "+toAdd+" Summoner Level: " + summonerObj[toSearch].summonerLevel+"</h1>");
     });
 });
 
 function search(searchItem) {
 	//return "This is the string from search function";
 	var xhr = new XMLHttpRequest();
-	var url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + searchItem + "?api_key=cc414580-c562-4f62-a74a-a1a8076e7a59";
+	var api_key = "cc414580-c562-4f62-a74a-a1a8076e7a59";
+	var url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + searchItem + "?api_key=" + api_key;
 	xhr.open("GET", url, false);
 	xhr.send();
+
 	//var myObject = JSON.stringify(search);
 	//var obj = JSON.parse(myObject);
-	var obj = JSON.parse(xhr.response);
+	//var obj = JSON.parse(xhr.response);
 	//var finalobj = JSON.parse(obj.response);
 
 	
-	return searchItem + ": " + obj[searchItem].id;
+	return xhr.response;
 	
 }
