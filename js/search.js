6a82c86583657c4abd5e489e9cdb39c4cc86b0c3
themantello <thechoy@gmail.com>
2014-09-12 17:07:39 -0400

    Added captal l

--- js/search.js
+++ js/search.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     $('button').click(function() {
     	var toSearch = $("input[name=searchitem]").val();
-    	var summonerObj = JSON.parse(search(toSearch));
-    	var sumIcon = summonerObj[toSearch].profileIconId; 
-    	var toAdd = summonerObj[toSearch].name;
-        $('#results').append("<h1> <img src=\"https://ddragon.leagueoflegends.com/cdn/4.16.1/img/profileicon/"+sumIcon+".png\"> "+toAdd+" Summoner Level: " + summonerObj[toSearch].summonerLevel+"</h1>");
+    	var inputName = toSearch.toLowerCase();
+    	var summonerObj = JSON.parse(search(inputName));
+    	var sumIcon = summonerObj[inputName].profileIconId; 
+    	var toAdd = summonerObj[inputName].name;
+        $('#results').append("<h1> <img src=\"https://ddragon.leagueoflegends.com/cdn/4.16.1/img/profileicon/"+sumIcon+".png\"> "+toAdd+" -- Summoner Level: " + summonerObj[inputName].summonerLevel+"</h1>");
     });
 });
 
@@ -12,6 +13,8 @@ function search(searchItem) {
 	//return "This is the string from search function";
 	var xhr = new XMLHttpRequest();
 	var api_key = "cc414580-c562-4f62-a74a-a1a8076e7a59";
+	// var test1 = String(searchItem);
+	// var test2 = test1.toLowerCase();
 	var url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + searchItem + "?api_key=" + api_key;
 	xhr.open("GET", url, false);
 	xhr.send();
